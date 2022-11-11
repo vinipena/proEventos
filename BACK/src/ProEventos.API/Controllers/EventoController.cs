17df6030b2f46ca77b584f2a55ec6eff24039917
vinipena <vinicius.pena@targetsistemas.com.br>
2022-11-11 16:31:19 -0300

    Seção 3 - Entity Framework - Migrations e DB

--- BACK/src/ProEventos.API/Controllers/EventoController.cs
+++ BACK/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -13,27 +14,12 @@ namespace ProEventos.API.Controllers
     public class EventoController : ControllerBase
     {
 
-    public IEnumerable<Evento> _evento = new Evento[]{
-                new Evento(){
-                EventoId=1,
-                Tema="Angular 11 e .Net5",
-                Local="São Paulo",
-                Lote= "1",
-                QtdPessoas =250,
-                DataEvento=DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImagemUrl="foto.png"},
-                  new Evento(){
-                EventoId=2,
-                Tema="Angular 12 e Bootstrap",
-                Local="São Paulo",
-                Lote= "3",
-                QtdPessoas =350,
-                DataEvento=DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                ImagemUrl="foto.png"}
-        };
+   
+        private readonly DataContext _context;
 
-        public EventoController()
+        public EventoController( DataContext context)
         {
+            _context = context;
 
         }
 
@@ -42,14 +28,16 @@ namespace ProEventos.API.Controllers
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
     
 
    [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(
+                evento => evento.EventoId == id
+            );
                     }
     
 
