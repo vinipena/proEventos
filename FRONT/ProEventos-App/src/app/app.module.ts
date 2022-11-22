f9a9a52248597381a11ccc4032ce0e064b95248d
vinipena <vinicius.pena@targetsistemas.com.br>
2022-11-22 10:40:50 -0300

    Seção 4  - Angular

---
+++ FRONT/ProEventos-App/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import {  HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [AppComponent, EventosComponent, PalestrantesComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
